f7a30ab6e8a4b787fc758efbf04fb6ac5fac89aa
ayelencaparra <caparraayelen@gmail.com>
2025-06-29 18:52:13 -0300

    Arrreglando img

--- src/components/categories/SubIndexCategories.tsx
+++ src/components/categories/SubIndexCategories.tsx
@@ -34,7 +34,7 @@ export function SubIndexCategories ({setSeccionActiva, seccionActiva, handleClea
                             Crear</li>
                     </ul>
                     <img 
-                    src="src/img/image 1.png" 
+                    src="/public/image 1.png" 
                     alt="Imagen de un botón off"
                     onClick={handleImageClick}
                     />
--- src/components/stock/SubIndexStock.tsx
+++ src/components/stock/SubIndexStock.tsx
@@ -35,7 +35,7 @@ export function SubIndexStock ({setSeccionActiva, handleClean, setMensajeExito, 
                     Crear orden de <br />compra</li> 
             </ul>
             <img 
-            src="src/img/image 1.png" 
+            src="/public/image 1.png" 
             alt="Imagen de un botón off"
             onClick={handleImageClick}
             />
